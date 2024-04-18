d30fcf107512e5c1df7e15c85172804a449a794c
Олег Силецкий <siletskiyoleg@gmail.com>
2024-04-18 09:31:21 +0300

    add category model

--- cat_logistics/catalog/models.py
+++ cat_logistics/catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Category(MPTTModel):
+    name = models.CharField('Название', max_length=50, db_index=True)
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        db_index=True,
+        related_name='children',
+        verbose_name='Родительская категория'
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ('name',)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
