d1fa9172360f2f881cdd0e7c6dce630271a18ee8
Олег Силецкий <siletskiyoleg@gmail.com>
2024-04-24 12:27:47 +0300

    add 404 and 500 views

---
+++ cat_logistics/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
