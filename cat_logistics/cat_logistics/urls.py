82918bc888df56ee52022be8bb56dfd235278b23
Олег Силецкий <siletskiyoleg@gmail.com>
2024-04-24 12:27:39 +0300

    add 404 and 500 handlers

--- cat_logistics/cat_logistics/urls.py
+++ cat_logistics/cat_logistics/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+handler404 = 'pages.views.page_not_found'
+
+handler500 = 'pages.views.server_error'
+
 urlpatterns = [
     path('', include('catalog.urls')),
     path('admin/', admin.site.urls),
